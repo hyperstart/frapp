b173162483da1595500289eeee36f1bc2e5bf701
Anthony Ferrando <anthony.ferrando@Braintribe>
2017-11-08 08:35:00 +0100

    Added tests for get()

---
+++ tests/get.test.ts
@@ -0,0 +1,29 @@
+import { get } from "../src"
+
+test("get with empty path", done => {
+  const obj = { value: "foo" }
+  expect(get(obj, [])).toBe(obj)
+  done()
+})
+
+test("get existing key", done => {
+  const obj = { value: "foo" }
+  expect(get({ val: { [5]: [1, 5, { obj }] } }, ["val", 5, 2, "obj"])).toBe(obj)
+  done()
+})
+
+test("get on array", done => {
+  const obj = { value: "foo" }
+  expect(get([1, 2, [3, [obj], 6]], [2, 1, 0])).toBe(obj)
+  done()
+})
+
+test("get with non existing key", done => {
+  expect(get({ foo: {} }, ["notExist"])).toBe(undefined)
+  done()
+})
+
+test("get with non existing key on array", done => {
+  expect(get([1, 2], [9])).toBe(undefined)
+  done()
+})
